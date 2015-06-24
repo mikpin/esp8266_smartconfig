89fd25463f54a0836a1a025e01099746139fb035
Michele <michelepinto77@gmail.com>
2015-06-24 07:02:15 +0200

    start gpio mode development

---
+++ app/driver/utils.c
@@ -0,0 +1,12 @@
+#include "espressif/esp_common.h"
+
+#include "utils.h"
+#include "user_config.h"
+
+void init_mode(){
+uint32 io_reg;
+io_reg = GPIO_PIN_REG(MODE_PIN);
+gpio_output_set(0,0,0,MODE_PIN);
+PIN_PULLUP_EN(io_reg);
+
+}
