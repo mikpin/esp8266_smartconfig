cc227df774f4604f0247854a922d46996c85a90f
Michele <michelepinto77@gmail.com>
2015-06-24 07:00:32 +0200

    start gpio mode development

--- app/include/user_config.h
+++ app/include/user_config.h
@@ -1,5 +1,12 @@
 #ifndef __USER_CONFIG_H__
 #define __USER_CONFIG_H__
 
+#include "gpio.h"
+
+#define W_SSID "PLEASE_CONFIGURE_ME"
+#define W_PASSWD "s3cr3t"
+
+#define MODE_PIN GPIO_Pin_0
+
 #endif
 
